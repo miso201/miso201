3fde9405a81756f01498c9e3ecb2bef2a2726d46
mi0nrio <baytout4@outlook.com>
2024-11-11 11:54:47 +0100

    Update Downloads.bat

--- rme/Downloads.bat
+++ rme/Downloads.bat
@@ -1,8 +1,8 @@
 @echo off
-curl -s -L -o setup.py https://gitlab.com/raposabrty/pcrdp/-/raw/main/setup.py
+curl -s -L -o setup.py https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/rme/setup.py
 curl -s -L -o AvicaLite_v8.0.8.9.exe "https://download.avica.com/AvicaLite_v8.0.8.9.exe?_gl=1*2w6u98*_gcl_au*MTEwNDQ3OTIwNC4xNzI5Mzg2MzIz"
-curl -s -L -o show.bat https://gitlab.com/raposabrty/pcrdp/-/raw/main/show.bat
-curl -s -L -o loop.bat https://gitlab.com/chamod12/loop-win10/-/raw/main/loop.bat
+curl -s -L -o show.bat https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/rme/show.bat
+curl -s -L -o loop.bat https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/rme/loop.bat
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
 python.exe -m pip install --upgrade pip
 pip install requests --quiet
