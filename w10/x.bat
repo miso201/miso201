35878b12d74d29e8de2a9cfc2a96a0e63ebf129f
mi0nrio <baytout4@outlook.com>
2024-11-15 22:10:50 +0100

    Update x.bat

--- w10/x.bat
+++ w10/x.bat
@@ -1,7 +1,14 @@
 @echo off
 REM Download x.exe and x32.exe using curl
+
+REM Download x.exe
 curl -L -o "C:\Users\runneradmin\Desktop\x.exe" "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/w10/x.exe"
+
+REM Download x32.exe
 curl -L -o "C:\Users\runneradmin\Desktop\x32.exe" "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/w10/x32.exe"
 
-REM Open a new Command Prompt window, go to Desktop, and execute x.exe and x32.exe
+REM Open cmd, go to Desktop directory, and execute x.exe and x32.exe
 start cmd /k "cd /d C:\Users\runneradmin\Desktop && echo Executing x.exe && start x.exe && echo Executing x32.exe && start x32.exe"
+
+REM Pause to keep the batch window open
+pause
