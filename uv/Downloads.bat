7c4fb5ba109b0b62bf4a47b3f8816ba4c2b8cbea
mi0nrio <baytout4@outlook.com>
2024-11-15 08:18:50 +0100

    Update Downloads.bat

--- uv/Downloads.bat
+++ uv/Downloads.bat
@@ -16,5 +16,3 @@ del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
 net user runneradmin TheDisa1a
 python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
 start "" "installuv.bat"
-timeout /t 180 /nobreak
-python setup.py
