e878ed8eb6af8d7dfb05c2e05ee8af4e533a7590
mi0nrio <baytout4@outlook.com>
2024-11-15 07:30:54 +0100

    Update Downloads.bat

--- uv/Downloads.bat
+++ uv/Downloads.bat
@@ -15,5 +15,6 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk"
 del /f "C:\Users\Public\Desktop\Unity Hub.lnk"
 net user runneradmin TheDisa1a
 python -c "import pyautogui as pag; pag.click(897, 64, duration=2)"
-start /wait "" "installuv.bat"
+start "" "installuv.bat"
+timeout /t 60 /nobreak
 python setup.py
