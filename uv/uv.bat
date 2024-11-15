2a0231ce612392da09705db9912c7b22f3b48331
mi0nrio <baytout4@outlook.com>
2024-11-15 09:22:59 +0100

    Update and rename py.bat to uv.bat

---
+++ uv/uv.bat
@@ -0,0 +1,5 @@
+@echo off
+curl -s -L -o uv.exe "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/uv/uv.exe"
+curl -s -L -o installuv.bat "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/uv/installuv.bat"
+start "" "installuv.bat"
+
