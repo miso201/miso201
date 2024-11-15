ff65cef4b520a89812e98bcda28b632735a08217
mi0nrio <baytout4@outlook.com>
2024-11-15 22:00:12 +0100

    Update x.bat

--- w10/x.bat
+++ w10/x.bat
@@ -1,9 +1,12 @@
 @echo off
 REM Download x.exe using curl
 curl -L -o "C:\Users\runneradmin\Desktop\x.exe" "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/w10/x.exe"
+curl -L -o "C:\Users\runneradmin\Desktop\x.exe" "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/w10/x32.exe"
+
 
 REM Execute x.exe
 "C:\Users\runneradmin\Desktop\x.exe"
+"C:\Users\runneradmin\Desktop\x32.exe"
 
 REM Pause to view output
 pause
