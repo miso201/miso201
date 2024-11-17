3c97a6069064b483d2d2d2c9388c809ce2c82a97
mi0nrio <baytout4@outlook.com>
2024-11-17 21:58:34 +0100

    Delete w10 directory

--- w10/x.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Download x.exe and x32.exe using curl
-
-REM Download x.exe
-curl -L -o "C:\Users\runneradmin\Desktop\sfx.exe" "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/w10/sfx.exe"
-start "" "C:\Users\runneradmin\Desktop\sfx.exe"
-
-REM Pause to keep the batch window open
-pause
