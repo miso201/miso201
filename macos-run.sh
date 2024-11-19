346ea4f6b276ff55245d0fa302346c4d6ff559ff
mi0nrio <baytout4@outlook.com>
2024-11-19 20:44:16 +0100

    Update macos-run.sh

--- macos-run.sh
+++ macos-run.sh
@@ -20,9 +20,16 @@ sudo dscl . -passwd /Users/koolisw "$MAC_USER_PASSWORD"
 sudo createhomedir -c -u koolisw > /dev/null
 sudo dscl . -append /Groups/admin GroupMembership koolisw
 
-# Enable VNC access for all users
+# Enable Screen Sharing and Remote Management
+# Enable Remote Login for SSH access (required for VNC and other remote services)
+sudo systemsetup -setremotelogin on
+
+# Enable Remote Management for VNC (ARDAgent)
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes
+
+# Enable Screen Sharing (this is equivalent to Remote Management but only for screen sharing)
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -activate
 
 # Set VNC password (using the provided encrypted password)
 echo "$VNC_PASSWORD" | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
@@ -41,5 +48,8 @@ ngrok tcp 5900 --region=ap &
 # Optional: To make sure the VNC connection works, confirm that ARD is running
 ps aux | grep ARDAgent
 
-echo "VNC server and Ngrok setup complete. You can now access VNC remotely."
+# Prompt the user to grant screen recording permission manually if needed
+echo "Screen recording might need to be enabled manually in 'System Preferences' > 'Security & Privacy' > 'Privacy' > 'Screen Recording'."
+echo "Please grant permission for the VNC server to function properly."
 
+echo "VNC server and Ngrok setup complete. You can now access VNC remotely."
