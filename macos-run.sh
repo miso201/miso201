c4d124ccb03d87d50086fc8febefc28029d02210
mi0nrio <baytout4@outlook.com>
2024-11-19 20:37:47 +0100

    Update macos-run.sh

--- macos-run.sh
+++ macos-run.sh
@@ -1,33 +1,45 @@
-#macos-run.sh MAC_USER_PASSWORD VNC_PASSWORD NGROK_AUTH_TOKEN MAC_REALNAME
+#!/bin/bash
 
-#disable spotlight indexing
+# Set variables
+MAC_USER_PASSWORD=$1
+VNC_PASSWORD=$2
+NGROK_AUTH_TOKEN=$3
+MAC_REALNAME=$4
+
+# Disable spotlight indexing to prevent excessive resource usage
 sudo mdutil -i off -a
 
-#Create new account
+# Create a new user account (koolisw)
 sudo dscl . -create /Users/koolisw
 sudo dscl . -create /Users/koolisw UserShell /bin/bash
-sudo dscl . -create /Users/koolisw RealName $4
+sudo dscl . -create /Users/koolisw RealName "$MAC_REALNAME"
 sudo dscl . -create /Users/koolisw UniqueID 1001
 sudo dscl . -create /Users/koolisw PrimaryGroupID 80
 sudo dscl . -create /Users/koolisw NFSHomeDirectory /Users/koolisw
-sudo dscl . -passwd /Users/koolisw $1
-sudo dscl . -passwd /Users/koolisw $1
+sudo dscl . -passwd /Users/koolisw "$MAC_USER_PASSWORD"
 sudo createhomedir -c -u koolisw > /dev/null
-sudo dscl . -append /Groups/admin GroupMembership username
+sudo dscl . -append /Groups/admin GroupMembership koolisw
 
-#Enable VNC
+# Enable VNC access for all users
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -allowAccessFor -allUsers -privs -all
-sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes 
+sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -configure -clientopts -setvnclegacy -vnclegacy yes
 
-echo $2 | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
+# Set VNC password (using the provided encrypted password)
+echo "$VNC_PASSWORD" | perl -we 'BEGIN { @k = unpack "C*", pack "H*", "1734516E8BA8C5E2FF1C39567390ADCA"}; $_ = <>; chomp; s/^(.{8}).*/$1/; @p = unpack "C*", $_; foreach (@k) { printf "%02X", $_ ^ (shift @p || 0) }; print "\n"' | sudo tee /Library/Preferences/com.apple.VNCSettings.txt
 
-#Start VNC/reset changes
+# Restart VNC service
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -restart -agent -console
 sudo /System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate
 
-#install ngrok
+# Install Ngrok via Homebrew
 brew install --cask ngrok
 
-#configure ngrok and start it
-ngrok authtoken $3
+# Configure and start Ngrok for remote access
+ngrok authtoken "$NGROK_AUTH_TOKEN"
 ngrok tcp 5900 --region=ap &
+
+# Optional: To make sure the VNC connection works, confirm that ARD is running
+ps aux | grep ARDAgent
+
+echo "VNC server and Ngrok setup complete. You can now access VNC remotely."
+
