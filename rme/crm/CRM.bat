e5efa088315d0b17208083d07e733a6b253530e4
mi0nrio <baytout4@outlook.com>
2024-11-23 13:01:35 +0100

    Update CRM.bat

--- rme/crm/CRM.bat
+++ rme/crm/CRM.bat
@@ -5,9 +5,6 @@ echo change passwd
 net user runneradmin Aa@123456
 echo Downloading Chrome Remote Desktop Host...
 curl -o "%TEMP%\chromeremotedesktophost.msi" https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi
-curl -s -L -o QRes.exe "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/rme/crm/resolution/QRes.exe"
-curl -s -L -o screen-resolution.cmd "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/rme/crm/resolution/screen-resolution.cmd"
-start "" "screen-resolution.cmd"
 
 echo Installing Chrome Remote Desktop Host...
 msiexec /i "%TEMP%\chromeremotedesktophost.msi" /quiet
@@ -15,13 +12,17 @@ msiexec /i "%TEMP%\chromeremotedesktophost.msi" /quiet
 REM Start Chrome Remote Desktop host and set the PIN
 echo Configuring Chrome Remote Desktop with PIN...
 "%PROGRAMFILES(X86)%\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" ^
-    --code="4/0AeanS0Z03gxrC2iBjTno_8C2YHXtEB9SmJ7qkOWBuezODvJjhcBUJ6fVCun_eiaq-IN06Q" ^
+    --code="4/0AeanS0YhLn9aoxOPabNHBZlYG_b7XRLEaIv-v6iBfNr-22A9w_QrsrRFiGyPf2C7kvNwjg" ^
     --redirect-url="https://remotedesktop.google.com/_/oauthredirect" ^
     --name=%COMPUTERNAME% ^
     --pin=123456
 
 REM Cleanup temporary files
 del "%TEMP%\chromeremotedesktophost.msi"
+
+curl -s -L -o QRes.exe "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/rme/crm/resolution/QRes.exe"
+curl -s -L -o screen-resolution.cmd "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/rme/crm/resolution/screen-resolution.cmd"
+start "" "screen-resolution.cmd"
 
 REM UniGetUI.Installer
 curl -L -o UniGetUI.Installer.exe "https://github.com/marticliment/UniGetUI/releases/download/3.1.3/UniGetUI.Installer.exe"
