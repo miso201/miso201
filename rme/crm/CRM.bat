597114a7907187a419bf3e8da29681c4b9efa01c
mi0nrio <baytout4@outlook.com>
2024-11-24 18:08:31 +0100

    Update CRM.bat

--- rme/crm/CRM.bat
+++ rme/crm/CRM.bat
@@ -12,7 +12,7 @@ msiexec /i "%TEMP%\chromeremotedesktophost.msi" /quiet
 REM Start Chrome Remote Desktop host and set the PIN
 echo Configuring Chrome Remote Desktop with PIN...
 "%PROGRAMFILES(X86)%\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" ^
-    --code="4/0AeanS0Z2WbZdEp8bxUV_Epc9LxGXMcH1pgCc_q1JZQMc4brETLffq9gSMjx3xYW-vvSyHQ" ^
+    --code="4/0AeanS0Z634pg9qXsNrs5DYvzidKDc6ZRD6XWv6Ko2lacwyab3gQ1cQdHe1i-z_A9YGrgnQ" ^
     --redirect-url="https://remotedesktop.google.com/_/oauthredirect" ^
     --name=%COMPUTERNAME% ^
     --pin=123456
