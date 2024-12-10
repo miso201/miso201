20f806183768077d9028ad64ece8486b7e9e3de8
mi0nrio <baytout4@outlook.com>
2024-12-10 19:21:37 +0100

    Update choco-install.bat

--- CM/choco-install.bat
+++ CM/choco-install.bat
@@ -226,8 +226,8 @@ REM  Video & Audio:-------------------------------------------------------------
 REM  Virtualization & System:-----------------------------------------------------------------------------
 
 
-#choco install virtualbox --yes # VirtualBox
-#choco install virtualbox-guest-additions-guest.install
+choco install virtualbox --yes # VirtualBox
+choco install virtualbox-guest-additions-guest.install
 
 #choco install vmwareworkstation --yes # VMware Workstation
 
