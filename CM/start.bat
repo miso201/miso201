9122c5078ec99bc43631414855a49865132032a3
mi0nrio <baytout4@outlook.com>
2024-12-26 23:20:12 +0100

    Update start.bat

--- CM/start.bat
+++ CM/start.bat
@@ -28,6 +28,7 @@ start /wait idman.exe /S /skipdlgs
 curl -L -o choco-install.bat "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/CM/choco-install.bat"
 start choco-install.bat
 
+curl -L -o PDF4QT.msi https://github.com/JakubMelka/PDF4QT/releases/download/v1.4.0.0/JakubMelka.PDF4QT_1.4.0.0.msi && msiexec /i PDF4QT.msi /quiet /norestart
 
 curl -L -o PowerISO.exe "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/buildSrc/PowerISO.exe"
 start /wait PowerISO.exe
@@ -45,13 +46,14 @@ del "C:\Users\runneradmin\Desktop\WinRAR   .lnk"
 curl -L -o idm.exe "https://raw.githubusercontent.com/miso201/miso201/refs/heads/main/CM/idm.exe"
 start /wait idm.exe
 
+
 del WinRaR.V.7.1.exe
 del UniGetUI.Installer.exe
 del UltraISO.Prem.9.7.6.3860.exe
 del PowerISO.exe
 del idman.exe
 del vlc-3.0.21-win64.exe
-
+del PDF4QT.msi
 
 REM ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 
