f97ccf0ff0820bd435c1ce69bda15cda24d5488d
mi0nrio <baytout4@outlook.com>
2025-01-08 22:44:35 +0100

    Update linux.sh

--- CM/linux.sh
+++ CM/linux.sh
@@ -1,1 +1,1 @@
-DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AanRRrtw00YtMlpsB1EdigSipqIOfA23uIajqqMU2AnPrUrRV6k59X-aZ68kR5pp1g_6rA" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname) --pin=123456
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AanRRrsw-wvaZhtgkzlzqz1vx04QdPc735-i8OUzEdGLFy3z4VJ20d96xDdMfn1lUmvxOQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname) --pin=123456
