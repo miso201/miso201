6ca98d51e626e08bb5b21c2fb0d4390507afe471
mi0nrio <baytout4@outlook.com>
2025-01-15 22:45:58 +0100

    Update linux.sh

--- CM/linux.sh
+++ CM/linux.sh
@@ -64,4 +64,4 @@ sudo apt-get install -f -y  # Resolve any missing dependencies
 
 
 
-DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AanRRrsCXcHagycN5GmK1ii5wWbFtxXdidbI7k7sAnrFYrGsNX2xyyAZivqqt8pgAEUJKw" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname) --pin=123456
+DISPLAY= /opt/google/chrome-remote-desktop/start-host --code="4/0AanRRru1G5OgxqhQwl5DTN7YZCBKUsRbgsxdz-j8FFUdMdbVzpmN30dANWGtajyOrtb9BQ" --redirect-url="https://remotedesktop.google.com/_/oauthredirect" --name=$(hostname) --pin=123456
