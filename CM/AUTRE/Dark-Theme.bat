31860f5f3292d3c4be635f33363e450d45ca2ca1
mi0nrio <baytout4@outlook.com>
2025-01-23 21:41:50 +0100

    Create Dark-Theme.bat

---
+++ CM/AUTRE/Dark-Theme.bat
@@ -0,0 +1,16 @@
+@echo off
+:: Enable Dark Mode for Windows Explorer
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t REG_DWORD /d 0 /f
+:: Enable Dark Mode for System UI (Start menu, taskbar, etc.)
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "SystemUsesLightTheme" /t REG_DWORD /d 0 /f
+:: Download the image from the URL
+powershell -Command "Invoke-WebRequest -Uri 'https://img.goodfon.com/original/1920x1080/c/50/windows-10-pirat-windows-temnye.jpg' -OutFile 'C:\Users\%username%\Pictures\wallpaper.jpg'"
+
+:: Set the downloaded image as wallpaper
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\Users\%username%\Pictures\wallpaper.jpg" /f
+
+:: Apply the wallpaper
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
+:: Restart Explorer to apply the changes
+taskkill /f /im explorer.exe
+start explorer.exe
