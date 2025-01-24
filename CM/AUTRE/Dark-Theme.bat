1d22dab65244465e822dbd5323160324f538e056
mi0nrio <baytout4@outlook.com>
2025-01-24 20:33:12 +0100

    Update Dark-Theme.bat

--- CM/AUTRE/Dark-Theme.bat
+++ CM/AUTRE/Dark-Theme.bat
@@ -4,10 +4,10 @@ REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "
 :: Enable Dark Mode for System UI (Start menu, taskbar, etc.)
 REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "SystemUsesLightTheme" /t REG_DWORD /d 0 /f
 :: Download the image from the URL
-powershell -Command "Invoke-WebRequest -Uri 'https://img.goodfon.com/original/1920x1080/c/50/windows-10-pirat-windows-temnye.jpg' -OutFile 'C:\Users\%username%\Pictures\wallpaper.jpg'"
+curl -L -o "C:\Users\%username%\Pictures\windows-10-pirat-windows-temnye.jpg" "https://img.goodfon.com/original/1920x1080/c/50/windows-10-pirat-windows-temnye.jpg"
 
 :: Set the downloaded image as wallpaper
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\Users\%username%\Pictures\wallpaper.jpg" /f
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\Users\%username%\Pictures\windows-10-pirat-windows-temnye.jpg" /f
 
 :: Apply the wallpaper
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
