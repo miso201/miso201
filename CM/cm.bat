c8f02e110cecdae61d7cc1b32be2ec33217b3321
mi0nrio <baytout4@outlook.com>
2025-05-19 18:30:00 +0100

    Update cm.bat

--- CM/cm.bat
+++ CM/cm.bat
@@ -1,5 +1,5 @@
 "%PROGRAMFILES(X86)%\Google\Chrome Remote Desktop\CurrentVersion\remoting_start_host.exe" ^
-    --code="4/0AUJR-x64BN02RWRWSSYHcD0kupvzu8CUErEDcTnVDayg2nMQXHN1QIQr6bzO9Tw5x91ttg" ^
+    --code="4/0AUJR-x4m1h-ZI_NMZDrDptu-O0jeqLc862hve1ydWhCiuT8gys5ZLc_W5p1CByPJQsnbeg" ^
     --redirect-url="https://remotedesktop.google.com/_/oauthredirect" ^
     --name=%COMPUTERNAME% ^
     --pin=123456
